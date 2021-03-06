4921709f2db826babc41c5a5bd591861fc4f78d9
Andrii Slobodian <73819571+sproandrii@users.noreply.github.com>
2021-03-06 19:19:23 +0200

    lazy loading imgs

--- script.js
+++ script.js
@@ -173,6 +173,33 @@ allSections.forEach(function (section) {
   section.classList.add('section--hidden');
 });
 
+// Lazy loading imgs
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  // rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
 // console.log(document.documentElement);
 // console.log(document.head);
 // console.log(document.body);
